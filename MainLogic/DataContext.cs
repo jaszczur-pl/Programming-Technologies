1642797cb78e8fcdec451fb9f742177302fc3e03
Małgorzata <44722439+malgorzatakucharska@users.noreply.github.com>
2018-11-20 18:25:09 +0100

    Add files via upload
    Dodatnie akcesorow get i set w klasach znajdujacych sie w Data.
    Dostanie klasy WypelnianieStalymi , wypelniajacej kolekcje stalymi wartosciami.

---
+++ MainLogic/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Zadanie1.Data;
+
+namespace Zadanie1.MainLogic
+{
+    public class DataContext
+    {
+        public List<Customer> customers;
+        public Dictionary<int, CD> cds;
+        public ObservableCollection<Event> events;
+        public List<CDState> cdStates;
+    }
+}
---
+++ MainLogic/DataFiller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadanie1.MainLogic
+{
+    public abstract class DataFiller
+    {
+        public abstract void Fill(DataContext context);
+    }
+}
