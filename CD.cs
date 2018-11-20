22c413bec53d1fa72e8ac5a9d2e037e5c55aa2d8
Małgorzata <44722439+malgorzatakucharska@users.noreply.github.com>
2018-11-20 18:11:39 +0100

    Add files via upload
    Dodatnie akcesorow get i set w klasach znajdujacych sie w Data.
    Dostanie klasy WypelnianieStalymi , wypelniajacej kolekcje stalymi wartosciami.

---
+++ CD.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Zadanie1.Data
+{
+    public class CD
+    {
+       public int id { get; set; }
+    }
+}
---
+++ DataRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadanie1.MainLogic
+{
+    class DataRepository
+    {
+        private DataContext data;
+        private DataFiller filler;
+
+        DataRepository(DataFiller filler) {
+            this.filler = DataFiller;
+        }
+    }
+}
