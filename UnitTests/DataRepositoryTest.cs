b08f8c65020abeb5af180c99f5a8c7e98fa87027
Maciej Jaszczura <maciej.jaszczura17@gmail.com>
2018-11-21 17:25:38 +0100

    Created Unit Tests project
    - Added DataRepositoryTest class
    - Added test initialization and first method which tests customer addition to list

---
+++ UnitTests/DataRepositoryTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadanie1.Data;
+using Zadanie1.MainLogic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DataRepositoryTest
+    {
+        DataContext data;
+        DataFiller filler;
+        DataRepository dataRepository;
+
+        [TestInitialize]
+        public void InitializeTests() {
+            data = new DataContext();
+            filler = new ConstantFiller();
+            dataRepository = new DataRepository(filler, data);
+        }
+
+        [TestMethod]
+        public void AddCustomerTest() {
+            int oldListSize = data.customers.Count;
+
+            Customer customer = new Customer() {
+                name = "Kamil",
+                surname = "Filipczak",
+                emailAddress = "kf@gmail.com",
+                age = 51
+            };
+
+            data.customers.Add(customer);
+            int newListSize = data.customers.Count;
+
+            //check if size of new and old list is different
+            Assert.AreNotEqual(oldListSize, newListSize);
+
+            //check if list contains object
+            Assert.IsTrue(data.customers.Contains(customer));
+        }
+
+    }
+}
---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81f3198a-279f-405c-ba8d-c535380456c5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
