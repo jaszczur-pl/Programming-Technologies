a0b1f6640ed6551af4174dfb36f7be5ee759dd61
Maciej Jaszczura <maciej.jaszczura17@gmail.com>
2018-11-29 22:06:55 +0100

    New test class - DataServiceTest

---
+++ UnitTests/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadanie1.Data;
+using Zadanie1.MainLogic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        DataService service;
+        DataRepository repository;
+        DataContext context;
+        DataFiller filler;
+
+        [TestInitialize]
+        public void InitializeTests() {
+            filler = new FillingFromFile();
+            context = new DataContext();
+            repository = new DataRepository(filler, context);
+
+            service = new DataService(repository);
+        }
+    }
+}
